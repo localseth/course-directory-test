35c5260653e6e3a3972a8e76efe12e325076f821
Seth M <97685638+localseth@users.noreply.github.com>
2022-09-06 16:33:15 -0500

    Updates

---
+++ precache-manifest.074aa3d61f4d3964f56b2811095c77cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6cae891d10b8ebb39f3",
+    "url": "/course-directory-test/static/css/main.4270122a.chunk.css"
+  },
+  {
+    "revision": "e6cae891d10b8ebb39f3",
+    "url": "/course-directory-test/static/js/main.e6cae891.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory-test/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "6fdd2706dd40007af550",
+    "url": "/course-directory-test/static/js/runtime~main.6fdd2706.js"
+  },
+  {
+    "revision": "8fa3abcefa80cfe33387ae57f011977d",
+    "url": "/course-directory-test/index.html"
+  }
+];
